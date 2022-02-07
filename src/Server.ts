b5829b85bca9cd07fc8d5a62092726984da7473a
takuma masaki <tb.masaki0531@gmail.com>
2022-02-07 15:33:29 +0200

    first commit

---
+++ src/Server.ts
@@ -0,0 +1,11 @@
+import express, { Express } from "express";
+
+class Server {
+  private readonly _app: Express;
+  private readonly _port: string;
+  private readonly _host: string;
+
+  constructor() {}
+}
+
+export { Server };
---
+++ src/typings/card/index.d.ts
@@ -0,0 +1,5 @@
+export interface Word {
+    id: string;
+    detectedText: string;
+    targetText: string;
+}
