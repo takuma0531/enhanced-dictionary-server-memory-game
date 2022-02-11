11892e61273bf0a32cac923fe4e1b496f8a05115
takuma masaki <tb.masaki0531@gmail.com>
2022-02-11 13:46:57 +0200

    add some to socketeventnames

--- src/enums/SocketEventNames.ts
+++ src/enums/SocketEventNames.ts
@@ -1,4 +1,10 @@
 export enum SocketEventNames {
   CONNECTION = "connection",
   DISCONNECT = "disconnect",
+  GAME_START = "game:start",
+  GAME_FINISH = "game:finish",
+  GAME_CLICK = "game:click",
+  GAME_FLIP = "game:flip",
+  GAME_UNFLIP = "game:unflip",
+  GAME_CHECK = "game:check",
 }
--- src/listeners/GameHandler.ts
+++ src/listeners/GameHandler.ts
@@ -1,6 +1,7 @@
 import { Socket, Server } from "socket.io";
 import { gameManager } from "../entities/GameManager";
 import { Word } from "../typings/word";
+import { SocketEventNames } from "../enums/SocketEventNames";
 
 class GameHandler {
   private _io: Server;
@@ -9,8 +10,8 @@ class GameHandler {
   public init(io: Server, socket: Socket) {
     this._io = io;
     this._socket = socket;
-    this._socket.on("game:start", this.onShuffle);
-    this._socket.on("game:click", this.onClick);
+    this._socket.on(SocketEventNames.GAME_START, this.onShuffle);
+    this._socket.on(SocketEventNames.GAME_CLICK, this.onClick);
   }
 
   public onShuffle(words: Word[]) {
@@ -23,18 +24,18 @@ class GameHandler {
     const game = gameManager.getGame(room);
 
     game.checkIfFlippable(wordCardOrderId) &&
-      this._io.emit("game:flip", wordCardOrderId);
+      this._io.emit(SocketEventNames.GAME_FLIP, wordCardOrderId);
 
     const currentlySelectedCards = game.click(wordCardOrderId);
 
     if (currentlySelectedCards.length !== 2) return;
 
     if (game.checkIfMatched()) {
-      this._io.emit("game:check", currentlySelectedCards);
+      this._io.emit(SocketEventNames.GAME_CHECK, currentlySelectedCards);
 
-      game.checkIfFinished() && this._io.emit("game:finish");
+      game.checkIfFinished() && this._io.emit(SocketEventNames.GAME_FINISH);
     } else {
-      this._io.emit("game:unflip", currentlySelectedCards);
+      this._io.emit(SocketEventNames.GAME_UNFLIP, currentlySelectedCards);
     }
   }
 }
